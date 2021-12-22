562211bc6c2da80d8fbc65337da96d2b087a6577
Dori <dorantdushi@gmail.com>
2021-12-22 22:37:47 +0100

    Second Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+ 
   
+
+
+
+
+  click() :void {
+    alert("You clicked me")
+  }
+
+
 }
